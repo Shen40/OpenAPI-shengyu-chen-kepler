ea0ee0dcd997b60875a061813043294609627e50
Shen Chen <shenchen0558@gmail.com>
2025-05-16 12:54:46 -0400

    API fetch completed

---
+++ index.js
@@ -0,0 +1,10 @@
+fetch('https://api.open-meteo.com/v1/forecast?latitude=40.4862&longitude=-74.4518&hourly=temperature_2m,precipitation_probability&temperature_unit=fahrenheit')
+    .then(response => {
+        return response.text(); 
+  })
+    .then(text => {
+    console.log(text)
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
