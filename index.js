be7e14855bed98bc5e0b622b184e4650472608a5
Shen Chen <shenchen0558@gmail.com>
2025-05-23 00:28:31 -0400

    work in progress

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-fetch('https://api.open-meteo.com/v1/forecast?latitude=40.4862&longitude=-74.4518&hourly=temperature_2m,precipitation_probability&temperature_unit=fahrenheit')
+fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&forecast_days=1&temperature_unit=fahrenheit')
     .then(response => {
-        return response.text(); 
+        return response.json(); 
   })
-    .then(text => {
-    console.log(text)
+    .then(data => {
+    console.log(data);
+
+    if (!data.hourly || !data.hourly.temperature_2m || !data.hourly.time) {
+      throw new Error("Missing temperature data");
+    }
+
+    const temp = data.hourly.temperature_2m;
+    const times = data.hourly.time;
+
+    const tempSection = document.querySelector("#temperature");
+    const tempList = tempSection.querySelector("ul");
+
+    for(let i=0;i<temp.length;i++){ 
+    let display = document.createElement("li"); 
+    display.innerText = `Time: ${times[i]}, Temp: ${temp[i]}°F`; 
+    tempList.appendChild(display);
+    }
+
   })
   .catch(error => {
     console.error('Error fetching data:', error);
